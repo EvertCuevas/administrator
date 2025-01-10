3fe986787ac7c83bcd1af3598fa28cbd91461e47
Evert Cuevas <evert.cuevas.espinoza@gmail.com>
2025-01-09 23:54:02 -0400

    Modificacion de imagenes de login y de usuario

--- app/Views/template/menu.php
+++ app/Views/template/menu.php
@@ -29,20 +29,20 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
-    <img src="assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light" style="color: #003D99;">AdminLTE 3</span>
-  </a>
+  <div class="brand-link">
+    <img src="assets/dist/img/salesiano.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <h4 class="h2"><strong>SIS</strong> Admin</h4>
+</div>
 
   <!-- Sidebar -->
   <div class="sidebar">
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <img src="assets/dist/img/user.png" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block" style="color: #003D99;">Alexander Pierce</a>
+        <h6><strong><?php echo session()->get('nombre'); ?></strong></h6>
       </div>
     </div>
 
